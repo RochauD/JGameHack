5d141adc8dc467715ca7b1bb49db7dfcd39342f1
Denis Rochau <rochau.dev@gmail.com>
2014-02-01 16:36:25 +0100

    Main Window

--- JGameHack/main.cpp
+++ JGameHack/main.cpp
@@ -7,7 +7,30 @@
 
 int main(int argc, char* argv[])
 {
+	// Create the main window
+	sf::RenderWindow window(sf::VideoMode(1600, 900), "JGameHack");
 	
+
+	// Start the game loop
+	while (window.isOpen())
+	{
+		// Process events
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// Close window : exit
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		// Clear screen
+		window.clear();
+
+		//Draw stuff in here
+
+		// Update the window
+		window.display();
+	}
+	return EXIT_SUCCESS;
 
 	return 0;
 }
